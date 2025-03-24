1181c74c56b0abb3b1677d3fdf6f3cde8ef8c265
Marcos Aurélio Carvalho <marcoscarvalho@mconsult.com.br>
2025-03-24 18:22:08 -0300

    Implementação inicial da tela de orçamentos e ajustes finos na home.

--- src/app/components/orcamentos/orcamentos.component.ts
+++ src/app/components/orcamentos/orcamentos.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { PoInfoModule, PoSearchModule, PoTableModule, PoTagType, PoTableColumn, PoButtonModule, PoWidgetModule, PoFieldModule, PoModule } from '@po-ui/ng-components';
+import { PoPageDynamicSearchModule, PoPageDynamicSearchLiterals, PoPageDynamicSearchFilters, PoPageDynamicTableModule } from '@po-ui/ng-templates';
 
 @Component({
   selector: 'app-orcamentos',
+  imports: [
+    PoInfoModule,
+    PoTableModule,
+    PoSearchModule,
+    PoButtonModule,
+    PoWidgetModule,
+    PoFieldModule,
+    PoModule,
+    PoPageDynamicSearchModule,
+    PoPageDynamicTableModule,
+  ],
   templateUrl: './orcamentos.component.html',
   styleUrl: './orcamentos.component.css'
 })
 export class OrcamentosComponent {
 
-  
+  constructor(){
 
+  }
+
+  addOrcamento() {
+
+  }
+  
 }
