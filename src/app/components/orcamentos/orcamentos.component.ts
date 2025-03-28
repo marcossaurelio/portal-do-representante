f27716c32688efab23abdd9d3ded5f8beeddb3bf
Marcos Aurélio Carvalho <marcoscarvalho@mconsult.com.br>
2025-03-28 17:13:28 -0300

    Aprimoramento da tela de orçamentos

--- src/app/components/orcamentos/orcamentos.component.ts
+++ src/app/components/orcamentos/orcamentos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PoInfoModule, PoSearchModule, PoTableModule, PoTagType, PoTableColumn, PoButtonModule, PoWidgetModule, PoFieldModule, PoModule, PoTableAction } from '@po-ui/ng-components';
+import { PoInfoModule, PoSearchModule, PoTableModule, PoTagType, PoTableColumn, PoButtonModule, PoWidgetModule, PoFieldModule, PoModule, PoTableAction, PoPageAction } from '@po-ui/ng-components';
 import { PoPageDynamicSearchModule, PoPageDynamicSearchLiterals, PoPageDynamicSearchFilters, PoPageDynamicTableModule } from '@po-ui/ng-templates';
 
 @Component({
@@ -23,18 +23,29 @@ export class OrcamentosComponent {
   public columns: Array<PoTableColumn> = [];
   public items: Array<any> = [];
 
-  public itemActions: Array<PoTableAction> = [
-    {action: this.modifyBudget.bind(this),  icon: 'an an-note-pencil',        label: 'Alterar',            disabled: false                         },
-    {action: this.modifyBudget.bind(this),  icon: 'an an-magnifying-glass',   label: 'Visualizar',         disabled: false                         },
-    {action: this.modifyBudget.bind(this),  icon: 'an an-copy',               label: 'Copiar',             disabled: false                         },
-    {action: this.modifyBudget.bind(this),  icon: 'an an-check',              label: 'Aprovar cotação',    disabled: !!this.isQuotation.bind(this) },
-    {action: this.modifyBudget.bind(this),  icon: 'an an-x',                  label: 'Rejeitar cotação',   disabled: !!this.isQuotation.bind(this) },
+  public readonly actions: Array<PoPageAction> = [
+    {label: 'Incluir', action: this.addBudget.bind(this), icon: 'an an-plus', disabled: false, visible: true}
+  ]
+  
+  public readonly itemActions: Array<PoTableAction> = [
+    {action: this.modifyBudget.bind(this),      icon: 'an an-note-pencil',        label: 'Alterar',            disabled: false                                          },
+    {action: this.viewBudget.bind(this),        icon: 'an an-magnifying-glass',   label: 'Visualizar',         disabled: false                                          },
+    {action: this.copyBudget.bind(this),        icon: 'an an-copy',               label: 'Copiar',             disabled: false                                          },
+    {action: this.approveQuotation.bind(this),  icon: 'an an-check',              label: 'Aprovar cotação',    disabled: (item: any) => !this.isPendingQuotation(item)  },
+    {action: this.rejectQuotation.bind(this),   icon: 'an an-x',                  label: 'Rejeitar cotação',   disabled: (item: any) => !this.isPendingQuotation(item)  },
   ]
 
+  public readonly filterColumns: Array<PoPageDynamicSearchFilters> = [
+    { property: 'budget',         label: 'Orçamento'      , type: 'label'},
+    { property: 'order',          label: 'Pedido'         },
+    { property: 'customer',       label: 'Cliente'        },
+    { property: 'inclusionDate',  label: 'Data Inclusão'  },
+  ]
+  
   constructor(){
 
   }
-
+  
   ngOnInit(): void {
     
     this.columns = this.getColumns();
@@ -45,55 +56,73 @@ export class OrcamentosComponent {
   public getColumns(): Array<PoTableColumn> {
     return [
       {
-        property: 'Situação Orçamento',
+        property: 'budgetStatus',
+        label: 'Situação Orçamento',
         type: 'label',
         labels: [
-          { value: 'CP',  type: PoTagType.Info,       label: 'Cotação pendente',      icon: true },
-          { value: 'CR',  type: PoTagType.Neutral,    label: 'Cotação rejeitada',     icon: true },
-          { value: 'PP',  type: PoTagType.Warning,    label: 'Pré pedido pendente',   icon: true },
-          { value: 'PR',  type: PoTagType.Danger,     label: 'Pré pedido rejeitado',  icon: true },
-          { value: 'PA',  type: PoTagType.Success,    label: 'Pré pedido aprovado',   icon: true },
+          { value: 'CP',  type: PoTagType.Info,       label: 'Cotação pendente',      icon: true  },
+          { value: 'CR',  type: PoTagType.Neutral,    label: 'Cotação rejeitada',     icon: false },
+          { value: 'PP',  type: PoTagType.Warning,    label: 'Pré pedido pendente',   icon: true  },
+          { value: 'PC',  type: PoTagType.Neutral,    label: 'Pré pedido cancelado',  icon: true  },
+          { value: 'PR',  type: PoTagType.Danger,     label: 'Pré pedido rejeitado',  icon: true  },
+          { value: 'PA',  type: PoTagType.Success,    label: 'Pré pedido aprovado',   icon: true  },
         ]
       },
       {
-        property: 'Situação Pedido',
+        property: 'orderStatus',
+        label: 'Situação Pedido',
         type: 'label',
         labels: [
           { value: 'F', type: PoTagType.Success,    label: 'Faturado',          icon: true  },
-          { value: 'O', type: PoTagType.Warning,    label: 'Aguardando Ordem',  icon: true },
-          { value: 'C', type: PoTagType.Info,       label: 'Em carregamento',   icon: true },
+          { value: 'O', type: PoTagType.Warning,    label: 'Aguardando Ordem',  icon: true  },
+          { value: 'C', type: PoTagType.Info,       label: 'Em carregamento',   icon: true  },
         ]
       },
-      {property: 'Orçamento'},
-      {property: 'Pedido'},
-      {property: 'Cliente'},
-      {property: 'Data Inclusão'},
+      {property: 'budget', label: 'Orçamento'},
+      {property: 'order', label: 'Pedido'},
+      {property: 'customer', label: 'Cliente'},
+      {property: 'inclusionDate', label: 'Data Inclusão'},
     ]
   }
 
   public getItems(): Array<any> {
-   return [
-    {'Situação Orçamento':'PA',   'Situação Pedido':'F',  'Orçamento':'000001',   'Pedido':'000856',  'Cliente':'M C SERVICOS DE TECNOLOGIA E GESTAO',  'Data Inclusão':'07/03/2025'},
-    {'Situação Orçamento':'PA',   'Situação Pedido':'C',  'Orçamento':'000002',   'Pedido':'000892',  'Cliente':'SERV SAL REFINARIA LTDA',              'Data Inclusão':'07/03/2025'},
-    {'Situação Orçamento':'PA',   'Situação Pedido':'O',  'Orçamento':'000005',   'Pedido':'000913',  'Cliente':'MC DISTRIBUIDORA LTDA',                'Data Inclusão':'08/03/2025'},
-    {'Situação Orçamento':'PR',   'Situação Pedido':' ',  'Orçamento':'000008',   'Pedido':'      ',  'Cliente':'MOSSORO ATACADO E VAREJO LTDA',        'Data Inclusão':'10/03/2025'},
-    {'Situação Orçamento':'PP',   'Situação Pedido':' ',  'Orçamento':'000010',   'Pedido':'      ',  'Cliente':'M C SERVICOS DE TECNOLOGIA E GESTAO',  'Data Inclusão':'10/03/2025'},
-    {'Situação Orçamento':'CP',   'Situação Pedido':' ',  'Orçamento':'000022',   'Pedido':'      ',  'Cliente':'FORTALEZA REFINADOS SA',               'Data Inclusão':'10/03/2025'},
-    {'Situação Orçamento':'CR',   'Situação Pedido':' ',  'Orçamento':'000025',   'Pedido':'      ',  'Cliente':'SERV SAL REFINARIA LTDA',              'Data Inclusão':'10/03/2025'},
-   ]
+    return [
+      {'budgetStatus':'PA',   'orderStatus':'F',  'budget':'000001',   'order':'000856',  'customer':'M C SERVICOS DE TECNOLOGIA E GESTAO',  'inclusionDate':'07/03/2025'},
+      {'budgetStatus':'PA',   'orderStatus':'C',  'budget':'000002',   'order':'000892',  'customer':'SERV SAL REFINARIA LTDA',              'inclusionDate':'07/03/2025'},
+      {'budgetStatus':'PA',   'orderStatus':'O',  'budget':'000005',   'order':'000913',  'customer':'MC DISTRIBUIDORA LTDA',                'inclusionDate':'08/03/2025'},
+      {'budgetStatus':'PR',   'orderStatus':' ',  'budget':'000008',   'order':'      ',  'customer':'MOSSORO ATACADO E VAREJO LTDA',        'inclusionDate':'10/03/2025'},
+      {'budgetStatus':'PP',   'orderStatus':' ',  'budget':'000010',   'order':'      ',  'customer':'M C SERVICOS DE TECNOLOGIA E GESTAO',  'inclusionDate':'10/03/2025'},
+      {'budgetStatus':'CP',   'orderStatus':' ',  'budget':'000022',   'order':'      ',  'customer':'FORTALEZA REFINADOS SA',               'inclusionDate':'10/03/2025'},
+      {'budgetStatus':'CR',   'orderStatus':' ',  'budget':'000025',   'order':'      ',  'customer':'SERV SAL REFINARIA LTDA',              'inclusionDate':'10/03/2025'},
+    ]
   }
 
-  private isQuotation(item: any): boolean {
-    return item['Situação Orçamento'] === 'CP';
+  private isPendingQuotation(item: any): boolean {
+    return item.budgetStatus === 'CP';
   }
-
-  addOrcamento() {
-
-  }
-
-  modifyBudget() {
+  
+  public addBudget() {
     
   }
   
+  public viewBudget() {
+    
+  }
+  
+  public copyBudget() {
+    
+  }
+  
+  public modifyBudget() {
+    
+  }
+  
+  public approveQuotation() {
+
+  }
+
+  public rejectQuotation() {
+
+  }
   
 }
